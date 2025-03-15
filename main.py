6412f489afb2008c7ef45bbc1429d6ba0069b6ee
ibrahim29asad <ibrahim.asad2019@gmail.com>
2025-03-15 17:20:00 -0600

    Quick Break but adding Multi Head Attention
    Multi Head Attention
    is what the model will use to break up the input into 3 differnet inputs for the model to work properly and take inputs and will alter allow for the project to converse

--- main.py
+++ main.py
@@ -78,9 +78,6 @@ class PositionalEncoding(nn.Module):
 # add and Norm 
 # 
 
-
-
-
 #Equation for normailization 
 # <x j = gamm/beta
 #gamma= xj - Uj
@@ -114,6 +111,28 @@ class FeedForwardBlock(nn.Module):
     def forward(self, x):
         # (Batch, Seq_Len, d_model) --> (Batch, Seq_Len, d_ff) --> (Batch, Seq_Len, d_model)
         return self.linear_2(self.dropout(self.dropout(torch.relu(self.linear_1(x)))))
+
+#Multi-Head Attention
+# 3 inputs Query, Key, Value
+class MultiHeadAttentionBlock(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h" # this creates the heads for each input so dk
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) #Wq
+        self.w_k = nn.Linear(d_model, d_model) #Wk
+        self.w_v = nn.Linear(d_model, d_model) #Wv
+
+        self.w_o = nn.Linear(d_model, d_model) #Wo
+        self.dropout = nn.Dropout(dropout) 
+    
+    def forware(self, q, k, v, mask):
+        
+
 
 age = 3
 print(f'tim is the age: {age}')
